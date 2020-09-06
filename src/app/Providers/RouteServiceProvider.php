640a7af605e7bedd47492ff8b3ee5b8d12f2719e
Filippo Sallemi <fsallemi@theearplatform.com>
2020-09-06 15:07:29 +0200

    updating views

--- src/app/Providers/RouteServiceProvider.php
+++ src/app/Providers/RouteServiceProvider.php
@@ -21,7 +21,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/home';
+    public const HOME = '/analyzer';
 
     /**
      * Define your route model bindings, pattern filters, etc.
